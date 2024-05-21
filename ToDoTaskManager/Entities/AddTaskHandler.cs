407926da2140597c0eefb42f308f82df0c51c7b4
kiper01 <kiper0328@mail.ru>
2024-05-21 22:34:47 +1000

    fix

--- ToDoTaskManager/Entities/AddTaskHandler.cs
+++ ToDoTaskManager/Entities/AddTaskHandler.cs
@@ -8,48 +8,46 @@ namespace ToDoTaskManager
         public async Task AddTaskAsync(string description, DateTime endTime, TasksList tasksList)
         {
             DateTime startTime = DateTime.Now;
-            Tasks newTask;
+            Tasks newTask = new Tasks
+            {
+                Id = Guid.NewGuid(),
+                Description = description,
+                StartTime = startTime,
+                EndTime = endTime,
+                Status = TaskStatus.InProgress
+            };
 
             try
             {
-                newTask = new Tasks
-                {
-                    Id = Guid.NewGuid(),
-                    Description = description,
-                    StartTime = startTime,
-                    EndTime = endTime,
-                    Status = TaskStatus.InProgress
-                };
-
                 await tasksList.AddTaskAsync(newTask);
             }
             catch
             {
                 Console.WriteLine("Ошибка добавления Задачи");
+                throw;
             }
         }
 
         public async Task AddTaskAsync(string description, DateTime endTime, TasksList tasksList, Tasks existingTask)
         {
-            Tasks newTask;
-
+            Tasks newTask = new Tasks
+            {
+                Id = existingTask.Id,
+                Description = description,
+                StartTime = existingTask.StartTime,
+                EndTime = endTime,
+                Status = existingTask.Status
+            };
             try
             {
-                newTask = new Tasks
-                {
-                    Id = existingTask.Id,
-                    Description = description,
-                    StartTime = existingTask.StartTime,
-                    EndTime = endTime,
-                    Status = existingTask.Status
-                };
-
-                await tasksList.UpdateTaskAsync(newTask);
+                    await tasksList.UpdateTaskAsync(newTask);
             }
             catch
             {
                 Console.WriteLine("Ошибка добавления Задачи");
+                throw;
             }
+            
         }
     }
 }
